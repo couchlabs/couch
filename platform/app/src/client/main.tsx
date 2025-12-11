880768810e8aae93467ac46ca65a10a89c005890
Nick Balestra-Foster <nick@balestrafoster.com>
2025-12-10 22:41:49 -0800

    flattened

--- platform/app/src/client/main.tsx
+++ platform/app/src/client/main.tsx
@@ -1,8 +1,13 @@
-import { App } from "@app-client/App"
+import { cdpConfig } from "@app-client/lib/cdpConfig"
+import { HomePage } from "@app-client/pages/HomePage"
+import { CDPReactProvider } from "@coinbase/cdp-react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "@app-client/main.css"
+
+const queryClient = new QueryClient()
 
 const rootElement = document.getElementById("root")
 if (!rootElement) {
@@ -11,6 +16,10 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <CDPReactProvider config={cdpConfig}>
+      <QueryClientProvider client={queryClient}>
+        <HomePage />
+      </QueryClientProvider>
+    </CDPReactProvider>
   </StrictMode>,
 )
